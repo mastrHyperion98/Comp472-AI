b63eecaca4c01d5493405632dd6ca496e29adca6
Steven <s_smith1998@outlook.com>
2020-05-22 00:37:39 -0400

    First Commit, find number of crime per block

---
+++ Assignment1/functions.py
@@ -0,0 +1,61 @@
+import geopandas as gpd
+import pandas as pd
+
+
+# This function creates a data frame that is composed of all the blocks defined by our stepping function and its
+# respective id
+def generate_grid(step, min_x, max_x, min_y, max_y):
+    num_block_y = round((max_y + step - min_y) / step, 0)
+    num_block_x = round((max_x - min_x) / step, 0)
+    block_id = []
+    id_counter = 0
+
+    lower_x = []
+    lower_y = []
+    upper_x = []
+    upper_y = []
+
+    x = min_x
+    y = min_y
+    while x <= max_x + step:
+        while y <= max_y + step:
+            lower_x.append(round(x, 3))
+            lower_y.append(round(y, 3))
+            upper_x.append(round(x + step, 3))
+            upper_y.append(round(y + step, 3))
+            y = y + step
+            block_id.append(id_counter)
+            id_counter = id_counter + 1
+        x = x + step
+        y = min_y
+
+    dataframe = pd.DataFrame(lower_x, columns=['lower_x'])
+    dataframe['lower_y'] = lower_y
+    dataframe['upper_x'] = upper_x
+    dataframe['upper_y'] = upper_y
+    dataframe['block_id'] = block_id
+    # then we can compute the crime_rate and add that to our block
+    return dataframe
+
+
+def assign_block_id(block_frame, crime_df):
+    # traverse the points one by on
+
+    block_id = []
+
+    for point in crime_df['geometry']:
+        id = block_frame[(block_frame.lower_x <= point.x) & (block_frame.upper_x > point.x) &
+                         (block_frame.lower_y <= point.y) & (block_frame.upper_y > point.y)]['block_id'].iloc[0]
+        block_id.append(id)
+
+    # return our block_id list
+    return block_id
+
+
+def compute_crime_rate(block_frame, crime_df):
+    crime_count_per_block = pd.DataFrame(crime_df['block_id'].value_counts().reset_index())
+    crime_count_per_block.columns = ['block_id', 'count']
+    merged = pd.merge(block_frame, crime_count_per_block, on='block_id')
+    # crime_count_per_block.sort_values(by=['block_id'])
+    # print(crime_count_per_block.head())
+    print(merged.head())
---
+++ Assignment1/main.py
@@ -0,0 +1,40 @@
+# Import files
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.colors
+from functions import compute_crime_rate, generate_grid, assign_block_id
+
+step = 0.002
+# imports as a pandas dataframe
+crime_df = gpd.read_file('data/crime_dt.shp')
+points = crime_df.centroid
+# get our x and y coordinates isolated
+x_coords = np.array(points.x)
+y_coords = np.array(points.y)
+# compute our maximum and minimum x and y coordinates
+min_x = x_coords.min()
+max_x = x_coords.max()
+min_y = y_coords.min()
+max_y = y_coords.max()
+# draw our map
+"""""
+map = crime_df.plot(color='yellow', marker="s")
+map.set_xticks(np.arange(min_x, max_x+0.002, 0.002))
+map.set_yticks(np.arange(min_y, max_y+0.002, 0.002))
+map.set_xticklabels(np.arange(min_x, max_x+0.002, 0.002),rotation=270)
+map.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+map.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+map.set_facecolor('xkcd:purple')
+plt.show()
+"""""
+block_frame = generate_grid(step, min_x, max_x, min_y, max_y)
+block_id = assign_block_id(block_frame, crime_df)
+crime_df['block_id'] = block_id
+compute_crime_rate(block_frame, crime_df)
+
+
+# Now we want to define to create a dataframe that contains the crime rate for every element in our blocks
+
