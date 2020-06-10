3bdf8fbb2abbf32e6287bdfcf711af8a97a3b781
Steven <s_smith1998@outlook.com>
2020-06-09 23:59:42 -0400

    Generate Vocabulary

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -8,11 +8,32 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Splits our data around two given years
+def split_data(data, year1, year2):
+    data_2018 = data[(data['Created At'].str.find(year1) >= 0)]
+    data_2019 = data[(data['Created At'].str.find(year2) >= 0)]
+    return data_2018, data_2019
+
+
+def generate_vocabulary(data, column):
+    lower_cased = data[column].str.lower()
+    vocabulary = []
+    for statement in lower_cased:
+        tokens = statement.split(' ')
+        for token in tokens:
+            vocabulary.append(token)
+
+    return np.unique(vocabulary)
+
+
 # main function
 def main():
     data = pd.read_csv('data/hns_2018_2019.csv')
-    print(data)
-    
+    # Divide our data into two partitions based on year
+    data_2018, data_2019 = split_data(data, '2018', '2019')
+    vocabulary = pd.DataFrame(generate_vocabulary(data_2018, 'Title'), columns=['word'])
+    print(vocabulary)
+
 
 if __name__ == "__main__":
     main()
