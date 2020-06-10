1c69ad0cab4d75d8fbb6d6fc98ad76ab38f7d41f
Steven <s_smith1998@outlook.com>
2020-06-09 23:02:37 -0400

    A2 created

---
+++ Assignment2/main.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------
+# Assignment 2
+# Written by Steven Smith 40057065
+# For COMP 472 Section -  – Summer 2020
+# --------------------------------------------------------
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# main function
+def main():
+    data = pd.read_csv('data/hns_2018_2019.csv')
+    print(data)
+    
+
+if __name__ == "__main__":
+    main()
