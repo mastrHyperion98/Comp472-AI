867a9471fac5389411cf1329d531313af166616a
Steven <s_smith1998@outlook.com>
2020-06-14 01:25:19 -0400

    Naive Bayes Classifier Progress and initial Integration

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -7,6 +7,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from nltk.tokenize import word_tokenize
+import math
 
 # Splits our data around two given years
 def split_data(data, year1, year2):
@@ -20,11 +22,9 @@ def generate_vocabulary(data, column):
     lower_cased = data[column].str.strip().str.lower()
     vocabulary = []
     for statement in lower_cased:
-        tokens = statement.split(' ')
+        tokens = word_tokenize(statement)
+
         for token in tokens:
-            # disregard empty strings
-            if not token:
-                continue
             vocabulary.append(token)
 
     return np.unique(vocabulary)
@@ -40,15 +40,16 @@ def generate_frequency_per_type(data, freq_column, column, type):
             if token in dict:
                 dict[token] = dict[token] + 1
             else:
-                dict[token] = 1 # Increase to 2 for smoothness
-    column_name = type+'_frequency'
+                dict[token] = 1  # Increase to 2 for smoothness
+    column_name = type + '_frequency'
     keys = list(dict.keys())
     values = list(dict.values())
-    dict = {'word':keys, column_name:values}
-    return pd.DataFrame(dict)
+    dictionary = {'word': keys, column_name: values}
+    return pd.DataFrame(dictionary)
 
 
 def generate_frequency_frame(data, vocabulary):
+    print('COMPUTING FREQUENCY...')
     freq_story = generate_frequency_per_type(data, 'Title', 'Post Type', 'story')
     freq_ask_hn = generate_frequency_per_type(data, 'Title', 'Post Type', 'ask_hn')
     freq_show_hn = generate_frequency_per_type(data, 'Title', 'Post Type', 'show_hn')
@@ -68,30 +69,98 @@ def probability_of(data, value, feature):
     num_observations = len(data[feature])
     num_entries = len(data[(data[feature]) == value])
 
-    return float(num_entries/num_observations)
+    return float(num_entries / num_observations)
 
 
 # get the probability of each word given a post type
 def conditional_probability(vocabulary, list_types, delta):
     # Probability = P(W|C) = (frequency of w in C) /  total number of words in C + delta * size of vocab
-
+    print('COMPUTING CONDITIONAL PROBABILITIES...')
     for type in list_types:
         probabilities = {}
-        total_numbers_of_words_in_c = vocabulary[type+'_frequency'].sum()
+        total_numbers_of_words_in_c = vocabulary[type + '_frequency'].sum()
         size_vocabulary = len(vocabulary)
         words = vocabulary['word'].tolist()
         for word in words:
             tmp = vocabulary[(vocabulary.word == word)]
-            tmp = tmp[type+'_frequency'].tolist()
+            tmp = tmp[type + '_frequency'].tolist()
             frequency = tmp.pop()
-            cp = (frequency + delta) / (total_numbers_of_words_in_c + (delta*size_vocabulary))
+            cp = (frequency + delta) / (total_numbers_of_words_in_c + (delta * size_vocabulary))
             probabilities[word] = cp
         # we need to merge probabilities into vocabulary
         keys = list(probabilities.keys())
         values = list(probabilities.values())
-        dict = pd.DataFrame({'word':keys, 'p(word | {})'.format(type):values})
+        dict = pd.DataFrame({'word': keys, 'p(word | {})'.format(type): values})
         vocabulary = pd.merge(vocabulary, dict, how='left', on='word')
     return vocabulary
+
+
+def vocabulary_to_file(vocabulary):
+    print('WRITING VOCABULARY TO FILE...')
+    filename = 'model-2018.txt'
+    file = open(filename, 'w', encoding='utf-8')
+    to_write = ''
+    for i in range(len(vocabulary)):
+        index = i + 1
+        word = vocabulary.at[i, 'word']
+        frequency_story = vocabulary.at[i, 'story_frequency']
+        frequency_ask = vocabulary.at[i, 'ask_hn_frequency']
+        frequency_show = vocabulary.at[i, 'show_hn_frequency']
+        frequency_poll = vocabulary.at[i, 'poll_frequency']
+        cp_story = vocabulary.at[i, 'p(word | story)']
+        cp_ask = vocabulary.at[i, 'p(word | ask_hn)']
+        cp_show = vocabulary.at[i, 'p(word | show_hn)']
+        cp_poll = vocabulary.at[i, 'p(word | poll)']
+        to_write = to_write + ('{}  {}  {}  {}  {}  {}  {}  {}  {}  {}\n'.format(index, word, frequency_story, cp_story,
+                                                                                 frequency_ask, cp_ask, frequency_show,
+                                                                                 cp_show, frequency_poll, cp_poll))
+    file.write(to_write)
+    file.close()
+    print('WRITING COMPLETE!')
+
+
+class Naives_Bayes:
+    def __init__(self):
+        self.X = None
+        self.y = None
+        self.prior = {}
+
+    def fit(self, X, y):
+
+        length = len(X)
+        for type in y:
+            self.prior[type] = len(X[X['Post Type'] == type]) / length
+
+        vocabulary = pd.DataFrame(generate_vocabulary(X, 'Title'), columns=['word'])
+        # use to perform left merge on word
+        vocabulary = generate_frequency_frame(X, vocabulary)
+        vocabulary = conditional_probability(vocabulary, y, 0.5)
+        vocabulary = vocabulary.sort_values(by=['word'])
+        vocabulary_to_file(vocabulary)
+        self.X = vocabulary
+        self.y = y
+
+    def predict(self, test):
+        # argmaxcjlog(P(cj)) + Σlog(P(wi|c))
+
+        for documents in test:
+            scores = {}
+            for target in self.y:
+                p_target = math.log10(self.prior[target])
+                tokens = word_tokenize(documents)
+                score = p_target
+                for token in tokens:
+                    data = self.X
+                    sub_frame = data[(data.word == token)]
+                    if len(sub_frame) == 0:
+                        continue
+                    else:
+                        sub_frame.reset_index(inplace=True)
+                        score = score + math.log10(sub_frame.at[0, 'p(word | {})'.format(target)])
+                scores[target] = score
+
+
+
 
 
 # main function
@@ -99,13 +168,10 @@ def main():
     data = pd.read_csv('data/hns_2018_2019.csv')
     # Divide our data into two partitions based on year
     data_2018, data_2019 = split_data(data, '2018', '2019')
-    list_types = np.unique(data_2018['Post Type'])
-    vocabulary = pd.DataFrame(generate_vocabulary(data_2018, 'Title'), columns=['word'])
-    # use to perform left merge on word
-    vocabulary = generate_frequency_frame(data_2018, vocabulary)
-    vocabulary = conditional_probability(vocabulary,list_types,0.5)
-    print(vocabulary)
-
+    list_types = np.unique(['Post Type']).tolist()
+    list_types.append('poll')
+    NB = Naives_Bayes()
+    NB.fit(data_2018, list_types)
 
 # Executes main function
 if __name__ == "__main__":
